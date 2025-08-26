0ead8cab649aad1102d5e8e0cd20738fd07979ba
taemin-ctrl <ktm0106@naver.com>
2025-08-26 09:52:44 +0900

    Add files via upload

---
+++ 0317_uart.srcs/sources_1/new/uart_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/03/17 14:38:27
+// Design Name: 
+// Module Name: uart_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_top(
+
+    );
+endmodule
