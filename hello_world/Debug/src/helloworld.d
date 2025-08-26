c5a58bed697e8ae26173a93dc515a12c14b66fab
taemin-ctrl <ktm0106@naver.com>
2025-08-26 10:07:02 +0900

    Add files via upload

---
+++ hello_world/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_printf.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_printf.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/bspconfig.h:
---
+++ hello_world/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_cache.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/mb_interface.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_assert.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h \
+ C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_cache.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/mb_interface.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_assert.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h:
+
+C:/harman_verilog/0513_Microblaze_GPIO/vitis/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h:
+
+../src/platform_config.h:
